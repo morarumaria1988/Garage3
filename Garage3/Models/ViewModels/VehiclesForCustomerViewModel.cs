5b09fefea9603912c28ad6fa9a5882f084796c5d
Mohamed Diaa <736736+MohamedDiaa@users.noreply.github.com>
2024-05-03 16:43:44 +0200

    show vehicles by user id

---
+++ Garage3/Models/ViewModels/VehiclesForCustomerViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Garage3.Models.Entities;
+
+namespace Garage3.Models.ViewModels
+{
+    public class VehiclesForCustomerViewModel
+    {
+        public class VehicleViewModel
+        {
+
+            public string RegistrationNumber { get; set; }
+            public string PersonalNumber { get; set; }
+            public string TypeName { get; set; }
+            public string VType { get; set; }
+            public string Color { get; set; }
+            public string Make { get; set; }
+            public int NumberOfWheels { get; set; }
+
+        }
+        public class CustomerViewModel {
+            public string PersonalNumber { get; set; }
+            public string FullName { get; set; }
+        }
+        public IEnumerable<VehicleViewModel> vehicleViewModels { get; set; }
+
+        public CustomerViewModel customer { get; set; }
+    }
+
+   
+}

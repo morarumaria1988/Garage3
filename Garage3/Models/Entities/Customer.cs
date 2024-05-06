ad43863d623cf29f06be25da7fa58935a5d8abe6
Mohamed Diaa <736736+MohamedDiaa@users.noreply.github.com>
2024-05-06 23:19:42 +0200

    Add Validation to Customer entity properties

--- Garage3/Models/Entities/Customer.cs
+++ Garage3/Models/Entities/Customer.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.ObjectModel;
+using Garage3.Validation;
 
 namespace Garage3.Models.Entities
 {
     public class Customer
     {
+        [CheckPersonalNumber]
         public string PersonalNumber { get; set; }
+        [CheckFirstName]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName => FirstName + " " + LastName;
---
+++ Garage3/Validation/CheckPersonalNumber.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage3.Validation
+{
+    public class CheckPersonalNumber: ValidationAttribute
+    {
+
+        public override bool IsValid(object? value)
+        {
+
+            if (value is string input) { 
+                
+                // 198010201330
+               
+                if(input.ToCharArray().Count() != 12) return false;
+
+                foreach(char c in input.ToCharArray())
+                {
+                    string t = c.ToString();
+
+                    if (!int.TryParse(t, out int _)) {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+    }
+}
